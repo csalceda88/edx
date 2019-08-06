7b35edf952ebe561761cabc635193c1a0a757637
csalceda88@gmail.com <csalceda88@gmail.com>
2019-08-05 21:57:54 -0700

    .

--- 2.1_if_statements_bidge_tax.c
+++ 2.1_if_statements_bidge_tax.c
@@ -14,3 +14,7 @@ int main(void)
         printf("Regular Tax\n%d\n", sum*2);
     return 0;
 }
+
+/*You arrive in front of a bridge that you must cross to reach a village before dark. Crossing the bridge is not free - the bridgekeeper asks you to roll two dice to determine the cost. You decide to write a program to verify that he is charging the right price.
+
+Your program should read two integers, between 1 and 6, representing the values of each die. If the sum is greater than or equal to 10, then you must pay a special fee (36 coins). Otherwise, you pay twice the sum of the values of the two dice. Your program must then display the text "Special tax" or "Regular tax" followed by the amount you have to pay on the next line.*/
--- 2.1_if_statements_tug_of_war.c
+++ 2.1_if_statements_tug_of_war.c
@@ -28,3 +28,15 @@ int main(void)
     printf("Total weight for team 2: %d\n", sumteam2);
     return 0;
 }
+
+/*You decide to bet on the final match of the Tug of War National Championship. 
+
+Prior to the match the names and weights of the players are presented, alternating by team (team 1 player 1, team 2 player 1, team 1 player 2, and so on). There is the same number of players on each side. You record the player weights as they are presented and calculate a total weight for each time to inform your bet. You write a C program to assist with this.
+
+Your program should first read an integer indicating the number of members per team. Then, the program should read the player weights (integers representing kilograms) alternating by team. 
+
+After calculating the total weight of each team, the program should display the text "Team X has an advantage" (replacing X with 1 or 2 depending on which team has a greater total weight).
+
+You will then display the text "Total weight for team 1:" followed by the weight of team 1, then "Total weight for team 2:" followed by the weight of team 2 (see example below).
+
+You are guaranteed that the two teams will not have the same total weight.*/
