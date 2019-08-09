bfbb61c10c7e75b71b10248e654ee1017ea96e22
csalceda88@gmail.com <csalceda88@gmail.com>
2019-08-08 17:27:21 -0700

    .

---
+++ 2.2_store_recipe_ingredients_in_array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int array[10];
+    int readValue = 0;
+    int quantity = 0;
+    int i;
+    for(i = 0; i < 10; i++)
+    {
+        scanf("%d", &readValue);
+        array[quantity] = readValue;
+        quantity = quantity + 1;
+    }
+    scanf("%d", &readValue);
+    printf("%d", array[readValue]);
+    return 0;
+}
+
+/* Your grandparents gave you a fantastic cooking recipe but you can never remember how much of each ingredient you have to use! There are 10 ingredients in the recipe and the quantities needed for each of them are given as input (in grams). Your program must read 10 integers (the quantities needed for each of the ingredients, in order) and store them in an array. It should then read an integer which represents an ingredient's ID number (between 0 and 9), and output the corresponding quantity. */
